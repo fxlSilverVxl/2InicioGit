3cebedbd076256e887a48b967404c8c95c8a05bf
Valerio <SilverVR@outlook.com>
2023-02-09 10:08:04 -0600

    No sirve aqui jaja

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'La cancion mas insana del oeste 🤑',
+        path: '../music/Song 1.mp3',
+        artist: 'Yo',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'La cancion menos insana del oeste 🥶',
+        path: '../music/Song 2.mp3',
+        artist: 'Yo',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'Ni tan insana ni tan insanant 😎',
+        path: '../music/Song 3.mp3',
+        artist: 'Yo',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Marco Music Pro 🥵',
+        path: '../music/Song 4.mp3',
+        artist: 'Marco Aurelio Silva',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'no se',
+        path: '../music/Song 5.mp3',
+        artist: 'no se',
+        cover: '../images/cover 5.png'
+    }
+]
